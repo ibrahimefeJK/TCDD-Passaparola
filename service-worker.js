abf76aad2ea24077ed13503749c21e63a2ffbbab
ibrahimefeJK <ibrahimefeJK@users.noreply.github.com>
2026-08-17 16:54:03 +0300

    Eski Hakkımızda uyarısını tamamen kaldır

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='tcdd-passaparola-v4';
+const CACHE='tcdd-passaparola-v5';
 const ASSETS=['./','./index.html','./about.html','./manifest.webmanifest','./css/style.css','./css/features.css','./css/about.css','./js/defaultQuestions.js','./js/storage.js','./js/gameEngine.js','./js/app.js','./js/features.js','./firebase-config.js','./assets/logo.png','./assets/logo192.png','./assets/logo512.png','./assets/favicon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
